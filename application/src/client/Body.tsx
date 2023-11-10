cf5172594d5e8647bcfe6f98a995adfe2cf9dfbd
Lukas Mast <lukasmast07@gmail.com>
2023-11-10 17:41:03 +0100

    Add main react components

---
+++ application/src/client/Body.tsx
@@ -0,0 +1,11 @@
+function Body() {
+  return (
+    <>
+      <main>
+        <h1>Here are the input fields</h1>
+      </main>
+    </>
+  );
+}
+
+export default Body;
---
+++ application/src/client/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <h1>Navigation Bar</h1>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
