70c0dfe1ee6b332ddf47630cb296e4b06fe779da
Chang Jin Lee <jinlee1@ethz.ch>
2023-11-14 00:55:13 +0100

    Mostly added functionality to Morning.

--- application/src/client/Morning/MorningBody.tsx
+++ application/src/client/Morning/MorningBody.tsx
@@ -13,7 +13,7 @@ function MorningInputBody() {
   return (
     <main>
       <div className="morning-body-container">
-        <div className="top-buttons">
+        <article className="top-buttons">
           <button
             className="mode-button"
             onClick={() => handleButtonClick("/evening")}
@@ -21,10 +21,50 @@ function MorningInputBody() {
             MODE: Day
           </button>
           <button className="submit-button">Submit</button>
-        </div>
+        </article>
+        <article className="cardList">
+          <CardWithSlider inputTitle="How was your sleep?" sliderList={["dead","bad","meh","ok","nice"]}/>
+          <CardWithSlider inputTitle="How often are your wakeups?" sliderList={["0","1","2","3",">3"]}/>
+          <CardWithSlider inputTitle="How often did you go to the bathroom?" sliderList={["0h","1h","2h",">3h","didn't"]}/>
+          <CardWithSelect inputTitle="Select all that apply" selectList={["Depressed","Angry","Hungry","Sad"]}/>
+        </article>
       </div>
     </main>
   );
+
+
+  // helper functions:
+
+  function CardWithSlider(props: {inputTitle : string, sliderList: Array<string>}){
+    return (
+      <form className="card">
+        <label htmlFor="damnInput">{props.inputTitle}</label><br />
+        <input type="range" id="sleepInput" min={1} max={5} step={1}/>
+        <div className="sliderList">
+          <span>{props.sliderList[0]}</span>
+          <span>{props.sliderList[1]}</span>
+          <span>{props.sliderList[2]}</span>
+          <span>{props.sliderList[3]}</span>
+          <span>{props.sliderList[4]}</span>
+        </div>
+      </form>
+    )
+  }
+
+  function CardWithSelect(props: {inputTitle : string, selectList : Array<string>}){
+    return (
+      <form className="card">
+        <label htmlFor="selectInput">{props.inputTitle}</label>
+        <div className="button-grid card">
+          {props.selectList.map((label) => (
+            <button key={label} className="habit-button">
+              {label}
+            </button>
+          ))}
+        </div>
+      </form>
+    )
+  }
 }
 
 export default MorningInputBody;
