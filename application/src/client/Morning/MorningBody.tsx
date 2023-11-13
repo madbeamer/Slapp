25cedb17abe07bda61dc7b0b47a4cd92d3199100
Chang Jin Lee <jinlee1@ethz.ch>
2023-11-13 19:17:59 +0100

    Add MorningBody Submit and Mode Button

--- application/src/client/Morning/MorningBody.tsx
+++ application/src/client/Morning/MorningBody.tsx
@@ -1,11 +1,28 @@
 import "../Morning/MorningBody.css";
+import { useNavigate } from "react-router-dom";
 // import { useState } from "react";
 // import { FaPlus } from "react-icons/fa";
 
 function MorningInputBody() {
+
+  const navigate = useNavigate();
+
+  const handleButtonClick = (path: string) => {
+    navigate(path);
+  };
   return (
     <main>
-      <div className="body-container"></div>
+      <div className="body-container">
+        <div className="top-buttons">
+          <button
+            className="mode-button"
+            onClick={() => handleButtonClick("/evening")}
+          >
+            MODE: Day
+          </button>
+          <button className="submit-button">Submit</button>
+        </div>
+      </div>
     </main>
   );
 }
