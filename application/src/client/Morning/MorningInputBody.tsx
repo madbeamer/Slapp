6b031dd8970dbea2cbe68a4f560c432a3500db77
Chang Jin Lee <jinlee1@ethz.ch>
2023-11-13 17:23:05 +0100

    Basic Skeleton for Morning done, no routing.

---
+++ application/src/client/Morning/MorningInputBody.tsx
@@ -0,0 +1,14 @@
+import "../Body.css";
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+
+function MorningInputBody(){
+    return (
+        <main>
+          <div className="body-container">
+          </div>
+        </main>
+      );
+}
+
+export default MorningInputBody
