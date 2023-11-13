5ec1c58f9958162f7e13949c7f472857c4668771
Chang Jin Lee <jinlee1@ethz.ch>
2023-11-13 17:26:31 +0100

    Add Morning page

--- application/src/client/Morning/MorningInputBody.tsx
+++ application/src/client/Morning/MorningInputBody.tsx
@@ -1,11 +1,12 @@
 import "../Body.css";
-import { useState } from "react";
-import { FaPlus } from "react-icons/fa";
+// import { useState } from "react";
+// import { FaPlus } from "react-icons/fa";
 
 function MorningInputBody(){
     return (
         <main>
           <div className="body-container">
+            
           </div>
         </main>
       );
