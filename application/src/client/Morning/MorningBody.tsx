0f868cf985d5d7a266310ebb409c5db387ee3786
Chang Jin Lee <jinlee1@ethz.ch>
2023-11-14 15:20:44 +0100

    Removed multiselect in Morning.

--- application/src/client/Morning/MorningBody.tsx
+++ application/src/client/Morning/MorningBody.tsx
@@ -1,69 +1,61 @@
 import "./MorningBody.css";
 import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
-// import { FaPlus } from "react-icons/fa";
 
 function MorningInputBody() {
-
   const navigate = useNavigate();
 
-  const handleButtonClick = (path: string) => {
+  const handleModeClick = (path: string) => {
     navigate(path);
   };
+
   return (
     <main>
       <div className="morning-body-container">
         <article className="top-buttons">
           <button
             className="mode-button"
-            onClick={() => handleButtonClick("/evening")}
+            onClick={() => handleModeClick("/evening")}
           >
             MODE: Day
           </button>
           <button className="submit-button">Submit</button>
         </article>
         <article className="cardList">
-          <CardWithSlider inputTitle="How was your sleep?" sliderList={["dead","bad","meh","ok","nice"]}/>
-          <CardWithSlider inputTitle="How often are your wakeups?" sliderList={["0","1","2","3",">3"]}/>
-          <CardWithSlider inputTitle="How often did you go to the bathroom?" sliderList={["0h","1h","2h",">3h","didn't"]}/>
-          <CardWithSelect inputTitle="Select all that apply" selectList={["Depressed","Angry","Hungry","Sad"]}/>
+          <CardWithSlider
+            inputTitle="How was your sleep?"
+            sliderList={["dead", "bad", "meh", "ok", "nice"]}
+          />
+          <CardWithSlider
+            inputTitle="How often are your wakeups?"
+            sliderList={["0", "1", "2", "3", ">3"]}
+          />
+          <CardWithSlider
+            inputTitle="How often did you go to the bathroom?"
+            sliderList={["0h", "1h", "2h", ">3h", "didn't"]}
+          />
         </article>
       </div>
     </main>
   );
 
-
   // helper functions:
 
-  function CardWithSlider(props: {inputTitle : string, sliderList: Array<string>}){
+  function CardWithSlider(props: {
+    inputTitle: string;
+    sliderList: Array<string>;
+  }) {
     return (
       <form className="card">
-        <label htmlFor="damnInput">{props.inputTitle}</label><br />
-        <input type="range" id="sleepInput" min={1} max={5} step={1}/>
+        <label htmlFor="damnInput">{props.inputTitle}</label>
+        <br />
+        <input type="range" id="sleepInput" min={1} max={5} step={1} />
         <div className="sliderList">
-          <span>{props.sliderList[0]}</span>
-          <span>{props.sliderList[1]}</span>
-          <span>{props.sliderList[2]}</span>
-          <span>{props.sliderList[3]}</span>
-          <span>{props.sliderList[4]}</span>
+          {props.sliderList.map((key, value) => {
+            return <span key={key}>{value}</span>;
+          })}
         </div>
       </form>
-    )
-  }
-
-  function CardWithSelect(props: {inputTitle : string, selectList : Array<string>}){
-    return (
-      <form className="card">
-        <label htmlFor="selectInput">{props.inputTitle}</label>
-        <div className="button-grid card">
-          {props.selectList.map((label) => (
-            <button key={label} className="habit-button">
-              {label}
-            </button>
-          ))}
-        </div>
-      </form>
-    )
+    );
   }
 }
 
