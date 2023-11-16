e90037edcd75d3bd9db593500daead69f4246dd2
Lukas Mast <lukasmast07@gmail.com>
2023-11-16 15:20:33 +0100

    Add sleep page component

---
+++ application/src/client/Sleep/Sleep.tsx
@@ -0,0 +1,18 @@
+import Layout from "../Layout";
+import Header from "../Header";
+import SleepBody from "./SleepBody";
+import Footer from "../Footer";
+
+function Sleep() {
+  return (
+    <>
+      <Layout>
+        <Header />
+        <SleepBody />
+        <Footer />
+      </Layout>
+    </>
+  );
+}
+
+export default Sleep;
---
+++ application/src/client/Sleep/SleepBody.tsx
@@ -0,0 +1,11 @@
+import "./SleepBody.css";
+
+function SleepBody() {
+  return (
+    <main className="blog-container">
+      <div>Sleep page here.</div>
+    </main>
+  );
+}
+
+export default SleepBody;
