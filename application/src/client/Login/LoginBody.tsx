cc3c188a6efddf9ce156e631cfbb73db148ad4bf
Lukas Mast <lukasmast07@gmail.com>
2023-11-20 21:49:58 +0100

    Add A-B switch to login body to toggle version

--- application/src/client/Login/LoginBody.tsx
+++ application/src/client/Login/LoginBody.tsx
@@ -1,9 +1,28 @@
 import "./LoginBody.css";
+import ReactSwitch from "react-switch";
 
-function LoginBody() {
+function LoginBody({
+  switchValue,
+  setSwitchValue,
+}: {
+  switchValue: string;
+  setSwitchValue: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const handleSwitchChange = (checked: boolean) => {
+    const value = checked ? "B" : "A";
+    setSwitchValue(value);
+    sessionStorage.setItem("switchValue", value);
+  };
+
   return (
     <main className="login-body-container">
-      <h1>This is the login page</h1>
+      <ReactSwitch
+        onChange={handleSwitchChange}
+        checked={switchValue === "B"}
+        checkedIcon={false}
+        uncheckedIcon={false}
+      />
+      <div>Prototype {switchValue} is selected.</div>
     </main>
   );
 }
