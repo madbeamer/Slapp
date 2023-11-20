c4f0261850c762703c84bf35e9f5c8eda5107e5e
Chang Jin Lee <jinlee1@ethz.ch>
2023-11-20 23:28:50 +0100

    Add EveningBody A.

--- application/src/client/Evening/EveningBody.tsx
+++ application/src/client/Evening/EveningBody.tsx
@@ -28,12 +28,14 @@ function EveningBody({ switchValue }: { switchValue: string }) {
   const [newButtonLabel, setNewButtonLabel] = useState("");
   const [isDeletingHabit, setDeletingHabit] = useState(false);
 
+  console.log(habits);
+
   return (
     <main className="evening-body-container">
       <div className="top-buttons">
         <button
           className="mode-button"
-          onClick={() => handleButtonClick("/morning")}
+          onClick={() => handleNavigationClick("/morning")}
         >
           MODE: Night
         </button>
@@ -42,13 +44,11 @@ function EveningBody({ switchValue }: { switchValue: string }) {
 
       <div className="habit-container">
         <h1>Select all that apply:</h1>
-        <div className="habit-body card">
-          {switchValue === "A" ? (
-            <WriteHabitList habits={habits} />
-          ) : (
-            <div>This is prototype A</div>
-          )}
-        </div>
+        {switchValue === "A" ? (
+          <WriteHabitListSlider habits={habits} />
+        ) : (
+          <WriteHabitListMulti habits={habits} />
+        )}
       </div>
 
       <div className="buttonContainer">
@@ -97,7 +97,74 @@ function EveningBody({ switchValue }: { switchValue: string }) {
     setDeletingHabit(true);
   }
 
-  function WriteHabitList(props: {
+  function handleNavigationClick(path: string) {
+    navigate(path);
+  }
+
+  function handleSaveButton() {
+    if (newButtonLabel.trim() !== "") {
+      setHabits((prev) => {
+        const m = new Map(prev);
+        m.set(newButtonLabel, { active: true, selected: false });
+        return m;
+      });
+      setNewButtonLabel("");
+      setIsAddingHabit(false);
+    } else {
+      setIsAddingHabit(false);
+    }
+  }
+
+  function handleHabitClick(e: React.MouseEvent) {
+    if (!(e.target instanceof HTMLElement)) return;
+
+    if (isDeletingHabit) {
+      setHabits((prev) => {
+        if (!(e.target instanceof HTMLElement)) return prev;
+        const m = new Map(prev);
+        m.delete(e.target.innerHTML);
+        return m;
+      });
+    } else {
+      if (e.target.classList.contains("selected"))
+        e.target.classList.remove("selected");
+      else e.target.classList.add("selected");
+    }
+  }
+
+  function MakeSliderButton(props: {
+    habitName: string;
+    deleting: boolean;
+    selected: boolean;
+  }) {
+    if (props.deleting) {
+      return (
+        <div className="habit-slider-container">
+          <button className="habit-slider deleting" onClick={handleHabitClick}>
+            {props.habitName}
+          </button>
+        </div>
+      );
+    } else if (props.selected) {
+      return (
+        <div className="habit-slider-container">
+          <button className="habit-slider selected" onClick={handleHabitClick}>
+            {props.habitName}
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="habit-slider-container">
+          <button className="habit-slider" onClick={handleHabitClick}>
+            {props.habitName}
+          </button>
+        </div>
+      );
+    }
+  }
+
+  function WriteHabitListSlider(props: {
     habits: Map<string, { active: boolean; selected: boolean }>;
   }) {
     const activeList = new Array<string>();
@@ -106,7 +173,43 @@ function EveningBody({ switchValue }: { switchValue: string }) {
     });
 
     return (
-      <>
+      <div className="habit-body slider">
+        {activeList.map((value, key) => {
+          if (!props.habits.get(value)?.active) return;
+          if (isDeletingHabit) {
+            return (
+              <MakeSliderButton
+                key={key}
+                habitName={value}
+                deleting={true}
+                selected={false}
+              />
+            );
+          } else {
+            return (
+              <MakeSliderButton
+                key={key}
+                habitName={value}
+                deleting={false}
+                selected={props.habits.get(value)!.selected}
+              />
+            );
+          }
+        })}
+      </div>
+    );
+  }
+
+  function WriteHabitListMulti(props: {
+    habits: Map<string, { active: boolean; selected: boolean }>;
+  }) {
+    const activeList = new Array<string>();
+    props.habits.forEach((value, key) => {
+      if (value.active) activeList.push(key);
+    });
+
+    return (
+      <div className="habit-body multi">
         {activeList.map((value, key) => {
           if (!props.habits.get(value)?.active) return;
           if (isDeletingHabit) {
@@ -140,43 +243,8 @@ function EveningBody({ switchValue }: { switchValue: string }) {
               </button>
             );
         })}
-      </>
+      </div>
     );
-  }
-
-  function handleButtonClick(path: string) {
-    navigate(path);
-  }
-
-  function handleSaveButton() {
-    if (newButtonLabel.trim() !== "") {
-      setHabits((prev) => {
-        const m = new Map(prev);
-        m.set(newButtonLabel, { active: true, selected: false });
-        return m;
-      });
-      setNewButtonLabel("");
-      setIsAddingHabit(false);
-    } else {
-      setIsAddingHabit(false);
-    }
-  }
-
-  function handleHabitClick(e: React.MouseEvent) {
-    if (!(e.target instanceof HTMLElement)) return;
-
-    if (isDeletingHabit) {
-      setHabits((prev) => {
-        if (!(e.target instanceof HTMLElement)) return prev;
-        const m = new Map(prev);
-        m.delete(e.target.innerHTML);
-        return m;
-      });
-    } else {
-      if (e.target.classList.contains("selected"))
-        e.target.classList.remove("selected");
-      else e.target.classList.add("selected");
-    }
   }
 }
 
