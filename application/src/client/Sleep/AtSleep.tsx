8f29b85e04c8d3782ae31b2500b357d7e3e14af8
Lukas Mast <lukasmast07@gmail.com>
2023-11-20 21:51:33 +0100

    Add static page after sleep is activated

---
+++ application/src/client/Sleep/AtSleep.tsx
@@ -0,0 +1,13 @@
+import "./AtSleep.css";
+import { FaBed } from "react-icons/fa";
+
+function AtSleep() {
+  return (
+    <main className="at-sleep-container">
+      <h1>Sleep Well!</h1>
+      <FaBed color={"#f2ac3c"} size={200} />
+    </main>
+  );
+}
+
+export default AtSleep;
