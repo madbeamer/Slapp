3bb98b3cc06de00d192e0198c0971016d72e89a8
Lukas Mast <lukasmast07@gmail.com>
2023-11-21 22:43:36 +0100

    Change base

--- application/vite.config.ts
+++ application/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/slapp/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 5777,
