cca6b0147f9aeefe03ffb65c63a377938536d05f
Lukas Mast <lukasmast07@gmail.com>
2023-11-21 23:11:32 +0100

    Change image path

--- application/src/client/Blog/BlogBody.tsx
+++ application/src/client/Blog/BlogBody.tsx
@@ -6,28 +6,28 @@ const articles = [
     content:
       "Read an article from Harvard Medical School about the benefits of exercising before sleep.",
     link: "https://www.health.harvard.edu/staying-healthy/does-exercising-at-night-affect-sleep#:~:text=The%20takeaway%3A%20Getting%20regular%20exercise,adopt%20a%20relaxing%20bedtime%20routine.",
-    image: "/gym-training.jpg",
+    image: "./gym-training.jpg",
   },
   {
     title: "Avoiding Blue Light Before Bedtime",
     content:
       "Read an article from the National Sleep Foundation about the effects of blue light on sleep.",
     link: "https://www.sleepfoundation.org/bedroom-environment/blue-light",
-    image: "/blue-light.jpg",
+    image: "./blue-light.jpg",
   },
   {
     title: "The Importance of Hydration for Better Sleep",
     content:
       "Read an article from the Cleveland Clinic about the importance of hydration for better sleep.",
     link: "https://health.clevelandclinic.org/drink-water-before-bed/",
-    image: "/drink-water.jpg",
+    image: "./drink-water.jpg",
   },
   {
     title: "Optimizing Sleep with a Cool Room Environment",
     content:
       "Read an article from the National Center for Biotechnology Information about the effects of room temperature on sleep.",
     link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5446217/",
-    image: "/cool-room.jpg",
+    image: "./cool-room.jpg",
   },
 ];
 
