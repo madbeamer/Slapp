bb294bd0ff77075a65dc09ef56a44fc3e7c85d1d
Lukas Mast <lukasmast07@gmail.com>
2023-11-22 20:04:27 +0100

    Change basename of BrowserRouter

--- application/src/client/main.tsx
+++ application/src/client/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/slapp">
+    <BrowserRouter basename="/slapp/application">
       <App />
     </BrowserRouter>
   </React.StrictMode>
